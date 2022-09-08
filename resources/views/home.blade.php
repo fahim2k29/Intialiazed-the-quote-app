2bd96604cf877a27fd970e1b724d25682b10efd7
fahim2k29 <fa009him@gmail.com>
2022-09-09 00:02:16 +0600

    set quotes into home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Quotes') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <ul>
+                        @foreach($data as $quote)
+                            <li>{{$quote}}</li>
+                        @endforeach
+                    </ul>
+                    <div>
+                        <button onClick="history.go(0);" class="btn btn-primary">Refresh</button>
+                    </div>
                 </div>
             </div>
         </div>
